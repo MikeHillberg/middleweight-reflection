d67210e483e3a77a211ed31256a014ef99b2e13e
Mike Hillberg <18429489+MikeHillberg@users.noreply.github.com>
2024-08-24 16:31:25 -0700

    In GetUnderlyingEnumType, special-case some System types

--- MiddleweightReflection/CustomAttributeTypeProvider.cs
+++ MiddleweightReflection/CustomAttributeTypeProvider.cs
@@ -20,7 +20,7 @@ namespace MiddleweightReflection
 
         MrType ICustomAttributeTypeProvider<MrType>.GetSystemType()
         {
-            if(this.Assembly.LoadContext.TryFindMrType("System.Type", out var type))
+            if (this.Assembly.LoadContext.TryFindMrType("System.Type", out var type))
             {
                 return type;
             }
@@ -41,7 +41,7 @@ namespace MiddleweightReflection
             }
 
             // This is a type in an assembly that's not loaded, create a fake type.
-            return MrType.CreateFakeType(name, assembly:null);
+            return MrType.CreateFakeType(name, assembly: null);
         }
 
         PrimitiveTypeCode ICustomAttributeTypeProvider<MrType>.GetUnderlyingEnumType(MrType type)
@@ -49,10 +49,34 @@ namespace MiddleweightReflection
             // Get the underlying type of an enum. The metadata reader needs this so that when it's
             // reading an attribute, it knows how many bytes to read out of the binary format.
 
-            if(type.IsFakeType)
+            if (type.IsFakeType)
             {
-                // This will be caught
-                throw new MrException($"Can't get the underlying type of a fake type ({type.GetPrettyFullName()})");
+                // In general if there's a referenced type that we don't have loaded, we fake it up.
+                // But for a fake enum type, the Reader really needs to know what size it is in order to read
+                // the binary data.
+                // Maybe we should pre-load some well-known .Net assemblies to handle this.
+                // But for now, just hard-coding a couple of frequent types.
+
+                const string callingConvention = "System.Runtime.InteropServices.CallingConvention";
+                var typeName = type.GetPrettyFullName();
+                if(typeName == callingConvention)
+                {
+                   return PrimitiveTypeCode.Int32;
+                }
+
+                const string attributeTargets = "System.AttributeTargets";
+                if(typeName == attributeTargets)
+                {
+                    // [Flags] are unsigned
+                    return PrimitiveTypeCode.UInt32;
+                }
+
+                var message = $"Can't get the underlying type of a fake type ({type.GetPrettyFullName()})";
+                Debug.WriteLine(message);
+
+                // We're in an MR stack frame now, above some SMR frames, and then MR
+                // So we're throwing from MR through SMR to an MR catch block.
+                throw new MrException(message);
             }
             Debug.Assert(type.IsEnum);
 
